43711ea20e7cd6e3a8285d15254000a4b23f6365
Pedro Franco Guidolin <pedrofrancoguidolin@gmail.com>
2026-02-09 14:54:06 -0300

    [TaskManager-4] Create DB Context and Migration to configure DataBase

---
+++ TaskManagerApi/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagerApi.Entities;
+
+namespace TaskManagerApi.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }        
+    
+    public DbSet<TaskItem> Tasks => Set<TaskItem>();
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<TaskItem>(entity =>
+        {
+            entity.HasKey(t => t.Id);
+            entity.Property(t => t.Title);
+            entity.Property(t => t.Description);
+            entity.Property(t => t.Status);
+        });
+    }
+}
+
+    
--- TaskManagerApi/Program.cs
+++ TaskManagerApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManagerApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=tasks.db"));
+
 
 var app = builder.Build();
 

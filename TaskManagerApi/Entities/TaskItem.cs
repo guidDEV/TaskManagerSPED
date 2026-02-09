e23afb98e31214f1ed3e1f7223ed132b011e4e02
guidDEV <pedrofrancoguidolin@gmail.com>
2026-02-09 14:02:24 -0300

    [TaskManager-3] Exclude Test Controller and Create Entity

---
+++ TaskManagerApi/Entities/TaskItem.cs
@@ -0,0 +1,17 @@
+﻿namespace TaskManagerApi.Entities;
+
+public enum TaskStatus
+{
+    Pending, 
+    InProgress, 
+    Completed
+}
+
+public class TaskItem
+{
+    public int Id { get; set; } 
+    public string Title { get; set; } = string.Empty; 
+    public string Description { get; set; } = string.Empty; 
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow; 
+    public TaskStatus Status { get; set; } = TaskStatus.Pending;
+}

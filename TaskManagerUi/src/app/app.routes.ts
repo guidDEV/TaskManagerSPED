9b6e7af49f5a42bf40fe1bf55e72f24e217a53b6
Pedro Franco Guidolin <pedrofrancoguidolin@gmail.com>
2026-02-10 22:18:41 -0300

    [TaskManager-9] Change Configs

--- TaskManagerUi/src/app/app.routes.ts
+++ TaskManagerUi/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { DashboardLayoutComponent } from './layout/dashboard-layout/dashboard-layout.component';
+import { TaskListComponent } from './features/tasks/pages/task-list/task-list.component';
+import { TaskFormComponent } from './features/tasks/pages/task-form/task-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: DashboardLayoutComponent,
+    children: [
+      { path: '', component: TaskListComponent },
+      { path: 'new', component: TaskFormComponent },
+      { path: 'edit/:id', component: TaskFormComponent }
+    ]
+  }
+];
